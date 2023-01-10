cad4da3417a2c71c3542cb559f76f4e42823aa87
len <len1017tk@gmail.com>
2023-01-10 20:32:23 +0900

    SpringBoot3.0.1にバージョンアップ、POM定義修正

--- src/main/java/com/example/helloworldsample/HelloWorldSampleApplication.java
+++ src/main/java/com/example/helloworldsample/HelloWorldSampleApplication.java
@@ -1,15 +1,17 @@
-package com.example.demo;
+package com.example.helloworldsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class HelloWorldSampleApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(HelloWorldSampleApplication.class, args);
 	}
 
 	@RequestMapping("/")
--- src/test/java/com/example/helloworldsample/HelloWorldSampleApplicationTests.java
+++ src/test/java/com/example/helloworldsample/HelloWorldSampleApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.helloworldsample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class HelloWorldSampleApplicationTests {
 
 	@Test
 	void contextLoads() {
